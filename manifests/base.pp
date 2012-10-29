a1371c7ef51a94022f5cf6eddf4b329f99800828
Philippe Lewin <philippe.lewin@gmail.com>
2012-10-29 19:43:24 +0100

    Move some ubuntu stuff in the right place

--- manifests/base.pp
+++ manifests/base.pp
@@ -8,8 +8,8 @@ class nagios::base {
   }
 
   package { 'nagios':
-    alias => 'nagios',
     ensure => present,
+    alias => 'nagios',
     /* TODO: Very dirty workaround (Package install fail if nagios objects are not present ) */
     require => Nagios_command['check-host-alive'],
   }
@@ -36,60 +36,60 @@ class nagios::base {
   }    
 
   file { 'nagios_commands_cfg':
-    path   => "${nagios::defaults::vars::int_nagios_cfgdir}/commands.cfg",
     ensure => present,
+    path   => "${nagios::defaults::vars::int_nagios_cfgdir}/commands.cfg",
     notify => Service['nagios'],
     mode   => 0644,
-    owner  => root,
-    group  => root,
+    owner  => 'root',
+    group  => 'root',
   }
 
   file { 'nagios_cgi_cfg':
-    path => "${nagios::defaults::vars::int_nagios_cfgdir}/cgi.cfg",
+    path   => "${nagios::defaults::vars::int_nagios_cfgdir}/cgi.cfg",
     source => [ "puppet:///modules/site-nagios/configs/${fqdn}/cgi.cfg",
                 "puppet:///modules/site-nagios/configs/${::operatingsystem}/cgi.cfg",
                 "puppet:///modules/site-nagios/configs/cgi.cfg",
                 "puppet:///modules/nagios/configs/${::operatingsystem}/cgi.cfg",
                 "puppet:///modules/nagios/configs/cgi.cfg" ],
-    mode => '0644',
-    owner => 'root',
-    group => 0,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
     notify => Service['httpd'],
   }
 
   file { 'nagios_private':
-    path => "${nagios::defaults::vars::int_nagios_cfgdir}/private/",
-    ensure => directory,
-    purge => true,
+    ensure  => directory,
+    path    => "${nagios::defaults::vars::int_nagios_cfgdir}/private/",
+    purge   => true,
     recurse => true,
-    notify => Service['nagios'],
+    notify  => Service['nagios'],
     require => Group['nagios'],
-    mode => '0750',
-    owner => 'root',
-    group => 'nagios',
+    mode    => '0750',
+    owner   => 'root',
+    group   => 'nagios',
   }
 
   file { 'nagios_private_resource_cfg':
-    path => "${nagios::defaults::vars::int_nagios_cfgdir}/private/resource.cfg",
-    source => [ "puppet:///modules/site-nagios/configs/${::operatingsystem}/private/resource.cfg.${::architecture}",
-                "puppet:///modules/nagios/configs/${::operatingsystem}/private/resource.cfg.${::architecture}" ],
-    notify => Service['nagios'],
+    path    => "${nagios::defaults::vars::int_nagios_cfgdir}/private/resource.cfg",
+    source  => [ "puppet:///modules/site-nagios/configs/${::operatingsystem}/private/resource.cfg.${::architecture}",
+                 "puppet:///modules/nagios/configs/${::operatingsystem}/private/resource.cfg.${::architecture}" ],
+    notify  => Service['nagios'],
     require => Group['nagios'],
-    owner => 'root',
-    group => 'nagios',
-    mode => '0640',
+    owner   => 'root',
+    group   => 'nagios',
+    mode    => '0640',
   }
 
   file { 'nagios_confd':
-    path => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/",
-    ensure => directory,
-    purge => true,
+    ensure  => directory,
+    path    => "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/",
+    purge   => true,
     recurse => true,
-    notify => Service['nagios'],
+    notify   => Service['nagios'],
     require => Group['nagios'],
-    mode => '0750',
-    owner => root,
-    group => nagios,
+    mode    => '0750',
+    owner   => 'root',
+    group   => 'nagios',
   }
 
   Nagios_command <<||>>
@@ -201,33 +201,11 @@ class nagios::base {
     group   => 0,
   }
 
-  /* Workaround Ubuntu */
-  file{[
-    "${nagios::defaults::vars::int_nagios_cfgdir}/resource.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/generic-service_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/contacts_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/hostgroups_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/timeperiods_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/generic-host_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/extinfo_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/localhost_nagios2.cfg", 
-    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/services_nagios2.cfg", 
-  ]:
-    ensure  => file,
-    content => '',
-    require => Group['nagios'],
-    mode    => 0644,
-    owner   => 'root',
-    group   => 'nagios',
-    notify  => Package['nagios'],
-  }
-
-
   # manage nagios cfg files
   # must be defined after exported resource overrides and cfg file defs
   file { 'nagios_cfgdir':
-    path    => "${nagios::defaults::vars::int_nagios_cfgdir}/",
     ensure  => directory,
+    path    => "${nagios::defaults::vars::int_nagios_cfgdir}/",
     recurse => true,
     purge   => true,
     notify  => Service['nagios'],
--- manifests/base/ubuntu.pp
+++ manifests/base/ubuntu.pp
@@ -1,3 +1,22 @@
 class nagios::base::ubuntu inherits nagios::base::debian {
-
+  /* Workaround Ubuntu */
+  file{[
+    "${nagios::defaults::vars::int_nagios_cfgdir}/resource.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/generic-service_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/contacts_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/hostgroups_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/timeperiods_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/generic-host_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/extinfo_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/localhost_nagios2.cfg", 
+    "${nagios::defaults::vars::int_nagios_cfgdir}/conf.d/services_nagios2.cfg", 
+  ]:
+    ensure  => file,
+    content => '',
+    require => Group['nagios'],
+    mode    => 0644,
+    owner   => 'root',
+    group   => 'nagios',
+    notify  => Package['nagios'],
+  }
 }
